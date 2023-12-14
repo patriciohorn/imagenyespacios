f7b2b5da5272d48e5fa3b7dfcd75e20083da1f07
Patricio Mena <luispatricio.mh@gmail.com>
2023-12-13 21:48:46 -0600

    updated navbar, hero section. Switch about and features section. Redesigned tabs

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -7,10 +7,12 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 // init Swiper:
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('.swiper1', {
   // configure Swiper to use modules
   modules: [Navigation, Pagination],
   pagination: {
+    dynamicBullets: true,
+    dynamicMainBullets: 5,
     el: '.swiper-pagination',
     clickable: true
   },
@@ -42,6 +44,44 @@ const swiper = new Swiper('.swiper', {
     }
   }
 });
+
+const swiper2 = new Swiper('.swiper2', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    enabled: false
+  },
+
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 12,
+  centeredSlides: true,
+  centeredSlidesBounds: true,
+  slidesPerView: 1.2,
+  loop: true,
+  loopAddBlankSlides: true,
+  a11y: {
+    prevSlideMessage: 'Previous slide',
+    nextSlideMessage: 'Next slide'
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    480: {
+      slidesPerView: 1.5,
+      spaceBetween: 24,
+      navigation: {
+        enabled: true
+      }
+    }
+  }
+});
+console.log(swiper2.slides);
 
 const btnNext = document.querySelectorAll('.swiper-button-next');
 const btnPrev = document.querySelectorAll('.swiper-button-prev');
