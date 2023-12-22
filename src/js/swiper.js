d32b96236ffb0672dd0e30dd650c07f17915d817
Patricio Mena <luispatricio.mh@gmail.com>
2023-12-22 09:46:29 -0600

    updated carpintery gallery and product feature section img

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -7,45 +7,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 // init Swiper:
-const swiper1 = new Swiper('.swiper1', {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination],
-  pagination: {
-    dynamicBullets: true,
-    dynamicMainBullets: 5,
-    el: '.swiper-pagination',
-    clickable: true
-  },
-  navigation: {
-    enabled: false
-  },
-
-  direction: 'horizontal',
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 12,
-  centeredSlides: true,
-  centeredSlidesBounds: true,
-  slidesPerView: 1.2,
-
-  a11y: {
-    prevSlideMessage: 'Previous slide',
-    nextSlideMessage: 'Next slide'
-  },
-
-  breakpoints: {
-    // when window width is >= 320px
-    480: {
-      slidesPerView: 1.5,
-      spaceBetween: 24,
-      navigation: {
-        enabled: true
-      }
-    }
-  }
-});
-
-const swiper2 = new Swiper('.swiper2', {
+const swiper = new Swiper('.swiper', {
   // configure Swiper to use modules
   modules: [Navigation, Pagination],
   pagination: {
@@ -59,12 +21,10 @@ const swiper2 = new Swiper('.swiper2', {
   direction: 'horizontal',
   loop: true,
   grabCursor: true,
-  spaceBetween: 12,
-  centeredSlides: true,
-  centeredSlidesBounds: true,
+  spaceBetween: 16,
+
   slidesPerView: 1.2,
-  loop: true,
-  loopAddBlankSlides: true,
+
   a11y: {
     prevSlideMessage: 'Previous slide',
     nextSlideMessage: 'Next slide'
@@ -73,22 +33,17 @@ const swiper2 = new Swiper('.swiper2', {
   breakpoints: {
     // when window width is >= 320px
     480: {
-      slidesPerView: 1.5,
-      spaceBetween: 24,
+      slidesPerView: 3,
+      spaceBetween: 32,
       navigation: {
         enabled: true
       }
     }
   }
 });
-console.log(swiper2.slides);
 
-const btnNext = document.querySelectorAll('.swiper-button-next');
-const btnPrev = document.querySelectorAll('.swiper-button-prev');
+const btnNext = document.querySelector('.swiper-button-next');
+const btnPrev = document.querySelector('.swiper-button-prev');
 
-btnNext.forEach((btn) =>
-  btn.addEventListener('click', (e) => e.target.parentNode.swiper.slideNext())
-);
-btnPrev.forEach((btn) =>
-  btn.addEventListener('click', (e) => e.target.parentNode.swiper.slidePrev())
-);
+btnNext.addEventListener('click', () => swiper.slideNext());
+btnPrev.addEventListener('click', () => swiper.slidePrev());
